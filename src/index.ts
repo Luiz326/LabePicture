dc7f39be7fc2ebd80cfa1349927bbe269d5f856b
Luiz <luizmitsuru@gmail.com>
2020-09-25 12:41:20 -0300

    deployatualizado

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/image", imageRouter);
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT, () => {
   if (server) {
     const address = server.address() as AddressInfo;
     console.log(`Servidor rodando em http://localhost:${address.port}`);
